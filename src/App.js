6fe28648fafe975199b5007636aa7fc610e38ff3
Shuhei Hagiwara <s.hagiwara@student.fontys.nl>
2022-10-12 11:33:34 +0200

    Setup basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import image from './img/bot.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="content">
+          <div className="header">
+            <div className="img">
+              <img src={image} alt=''/>
+            </div>
+            <div className="right">
+              <div className="name">Fontys BOT</div>
+              <div className="description">Nicest Fontys HelpDesk Service Employee</div>
+            </div>
+          </div>
+          <div className="main">
+            <div className="main_content">
+              <div className="messages">
+                <div className="bot-message" id="message1"></div>
+                <div className="human-message" id="message2"></div>
+              </div>
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="btm">
+              <div className="input">
+                <input type="text" id="input" placeholder="Enter a question" />
+              </div>
+              <div className="btn">
+                  <button>Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
