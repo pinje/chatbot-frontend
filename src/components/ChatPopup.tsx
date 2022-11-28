1a60519d6ea15f32ffc230a5fbfa9828bbf7be86
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-11-28 17:17:57 +0100

    reset attempt #1

--- src/components/ChatPopup.tsx
+++ src/components/ChatPopup.tsx
@@ -1,10 +1,9 @@
-import { popupState } from '../models/popupState';
-import './ChatPopup.css';
-import React, {useEffect, useState} from 'react';
+import { popupState } from "../models/popupState";
+import "./ChatPopup.css";
+import React, { useEffect, useState } from "react";
 
 //import settings
-import Settings from './Settings';
-
+import Settings from "./Settings";
 
 // import redux components
 import { Provider } from "react-redux";
@@ -14,10 +13,11 @@ import store from "../store";
 import Chat from "./chat/Chat";
 
 // import action
-import { createSession } from "./actions/watson";
+import { clearStore, createSession } from "./actions/watson";
 
 // import axios
 import axios from "axios";
+import { RESET_STATE } from "./actions/types";
 
 if (localStorage.session) {
   delete axios.defaults.headers.common["session_id"];
@@ -27,40 +27,46 @@ if (localStorage.session) {
 }
 
 function ChatPopup(props: popupState) {
+  const [openSettings, setOpenSettings] = useState<boolean>(false);
+  useEffect(() => {
+    // check if there's a session
+    if (!localStorage.session) {
+      // create
+      createSession();
+    }
+  });
 
-   const [openSettings, setOpenSettings] = useState<boolean>(false);
-   useEffect(() => {
-      // check if there's a session
-      if (!localStorage.session) {
-         // create
-         createSession();
-      }
-   })
+  return (
+    <Provider store={store}>
+      <div className="form-container">
+        <div className="popup-header">
+          <img className="bot-icon" src={require("../img/chat-profile.jpg")} />
+          <button onClick={() => props.setIsOpen(false)}> X </button>
 
-   return (
-      <Provider store={store}>
-      <div className='form-container'>
-         <div className='popup-header'>            
-            <img className='bot-icon' src={require('../img/chat-profile.jpg')} />
-            <button onClick={() => props.setIsOpen(false)}> X </button>
-             
-            <button onClick={() => {
-                  setOpenSettings(!openSettings)
-                  }}>
-               <img className='setting-icon' src={require('../img/setting.png')}            
-               
-               />
-            </button>
-            {openSettings && <Settings/>}
+          <button
+            onClick={() => {
+              setOpenSettings(!openSettings);
+            }}
+          >
+            <img className="setting-icon" src={require("../img/setting.png")} />
+          </button>
+          {openSettings && <Settings />}
+          {/* this doesn't get called for some reason idk */}
+          <button onClick={() => clearStore}>
+            <img
+              className="refresh-icon"
+              src={require("../img/refresh.png")}
+            ></img>
+          </button>
 
-            <div className='popup-header-box'>
-               <div className='popup-header-title'>David</div>
-               <div className='popup-header-description'>Fontys Help-Desk</div>
-            </div>
-         </div>        
-         <div className='title'>         
-            <Chat/>
-         </div>
+          <div className="popup-header-box">
+            <div className="popup-header-title">David</div>
+            <div className="popup-header-description">Fontys Help-Desk</div>
+          </div>
+        </div>
+        <div className="title">
+          <Chat />
+        </div>
       </div>
     </Provider>
   );
--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -1,8 +1,18 @@
 // import dependencies
-import {combineReducers} from "redux"
+import { combineReducers } from "redux";
+import { RESET_STATE } from "../components/actions/types";
 
 // import reducers
-import watson from "./watson"
+import watson from "./watson";
 
 // export combined reducers
-export default combineReducers({ watson })
+const appReducer = combineReducers({ watson });
+
+export const rootReducer = (state: any, action: any) => {
+  if (action.type === RESET_STATE) {
+    return appReducer(undefined, action);
+  }
+  return appReducer(state, action);
+};
+
+export default appReducer;
