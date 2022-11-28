8dab52b50ee54b1ec278e80f2fda18d128b3a819
Tsvetislav Rangelov <cveti2002mnt@gmail.com>
2022-11-28 15:49:33 +0100

    FAQ search feature

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,12 +1,19 @@
 import { useState, useEffect, useRef } from "react";
 import { connect } from "react-redux";
-import "./Chat.css"
+import "./Chat.css";
 import Category from "./Category";
 import Questions from "./Questions";
 import Question from "./Question";
 
 // import action
-import { userMessage, sendMessage, searchGoogle, askCategory, categoryList, askQuestion } from "../actions/watson";
+import {
+  userMessage,
+  sendMessage,
+  searchGoogle,
+  askCategory,
+  categoryList,
+  askQuestion,
+} from "../actions/watson";
 
 const Chat = ({
   chat,
@@ -28,7 +35,7 @@ const Chat = ({
   //Handle User Message
   const [message, setMessage] = useState("");
   const [toggleSearch, setToggleSearch] = useState(false);
-  
+
   // function that handles user submission
   const handleClick = (e: any) => {
     e.preventDefault();
@@ -55,87 +62,106 @@ const Chat = ({
   /*  When a category is chosen from the list,
       a message is sent as a user that asks the question (to personalize the experience),
       then the bot replies with Q&A of that topic */
-  const clickCategory = (category:string) => {
-    switch(category) {
-      case 'password':
+  const clickCategory = (category: string) => {
+    switch (category) {
+      case "password":
         return (
           userMessage("Let me see FAQs about password resets."),
           askCategory(category)
         );
-      case 'office':
+      case "office":
         return (
           userMessage("Let me see FAQs about Office 365."),
           askCategory(category)
         );
-      case 'equipment':
-      return (
-        userMessage("Let me see FAQs about Fontys equipment."),
-        askCategory(category)
-      );
-      case 'wifi':
+      case "equipment":
         return (
-          userMessage("Let me see FAQs about wifi."),
+          userMessage("Let me see FAQs about Fontys equipment."),
           askCategory(category)
         );
-      case 'media':
+      case "wifi":
+        return (
+          userMessage("Let me see FAQs about wifi."), askCategory(category)
+        );
+      case "media":
         return (
           userMessage("Let me see FAQs about audio and video."),
           askCategory(category)
         );
       default:
-        return(null);
+        return null;
     }
-  }
+  };
 
+  // good shit shuhei - Tsvetislav
   // (category already chosen) user clicks on a question, the question is sent as a user (to personalize the experience),
   // then the bot replies with the answer
-  const clickQuestion = (question:string) => {
-    return (
-      userMessage(question),
-      askQuestion(question)
-    );
-  }
+  const clickQuestion = (question: string) => {
+    return userMessage(question), askQuestion(question);
+  };
 
-  // Return button to see FAQ Category List 
+  // Return button to see FAQ Category List
   const returnCategoryMenu = () => {
     categoryList();
-  }
+  };
 
   // Check output on chat: link, FAQ category list, Specific category questions list, normal message
-  function condition(msg:any) {
-    switch(msg.type) {
-      case 'botLink':
-        return (<a href={msg.message}>{msg.message}</a>);
-      case 'category-list':
+  function condition(msg: any) {
+    switch (msg.type) {
+      case "botLink":
+        return <a href={msg.message}>{msg.message}</a>;
+      case "category-list":
         return (
           <div className="bot">
-              <Category clickCategory={clickCategory} />
+            <Category clickCategory={clickCategory} />
           </div>
         );
-      case 'category':
-        return (<div className="bot">
-                  <Questions category={msg.message} clickQuestion={clickQuestion}/>
-                  <button className="goback-button" onClickCapture={returnCategoryMenu}><img className="arrow-left" src={require('../../img/arrow-left.png')} /> Return to FAQ List</button>
-                </div>);
-      case 'question':
-        return (<div className="bot">
-                  <Question question={msg.message}/>
-                  <button className="goback-button" onClickCapture={returnCategoryMenu}><img className="arrow-left" src={require('../../img/arrow-left.png')} /> Return to FAQ List</button>
-                </div>);
+      case "category":
+        return (
+          <div className="bot">
+            <Questions category={msg.message} clickQuestion={clickQuestion} />
+            <button
+              className="goback-button"
+              onClickCapture={returnCategoryMenu}
+            >
+              <img
+                className="arrow-left"
+                src={require("../../img/arrow-left.png")}
+              />{" "}
+              Return to FAQ List
+            </button>
+          </div>
+        );
+      case "question":
+        return (
+          <div className="bot">
+            <Question question={msg.message} />
+            <button
+              className="goback-button"
+              onClickCapture={returnCategoryMenu}
+            >
+              <img
+                className="arrow-left"
+                src={require("../../img/arrow-left.png")}
+              />{" "}
+              Return to FAQ List
+            </button>
+          </div>
+        );
       default:
-        return (<div> {msg.message} </div>);
+        return <div> {msg.message} </div>;
     }
   }
 
   return (
     <div className="chat">
-      {/* sorry about this being so ugly guys lol, I know you'll take care of it, thanks in advance - Tsvetislav */}
       <button className="google-button" onClick={handleSearchToggle}>
-      {toggleSearch === false
-          ? <div>Google search</div>
-          :<div>Bot search</div> }
-        
-        </button>
+        {toggleSearch === false ? (
+          <div>Google search</div>
+        ) : (
+          <div>Bot search</div>
+        )}
+      </button>
       {/* Handle Messages */}
       <div className="history-box">
         <div className="intro-container">
@@ -145,29 +171,28 @@ const Chat = ({
 
         {/* Showing FAQ by categories*/}
         <div className="bot">
-            <Category clickCategory={clickCategory} />
+          <Category clickCategory={clickCategory} />
         </div>
-        
+
         {/* Display Chat */}
         {chat.length === 0
           ? ""
           : chat.map((msg: any) => (
               <div className={msg.type}>{condition(msg)}</div>
-              
             ))}
         <div ref={messagesEndRef} className="chat-buffer" />
       </div>
       {/* Input Box */}
       <div>
         <form onSubmit={handleClick} className="input-box">
-        <input
-          id="chatBox"
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-          placeholder="Enter a question...">         
-        </input>
-        <button> Send </button>     
-         {/* <img className="send-icon" src={require("../../img/sendicon.png")} /> */}
+          <input
+            id="chatBox"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            placeholder="Enter a question..."
+          ></input>
+          <button> Send </button>
+          {/* <img className="send-icon" src={require("../../img/sendicon.png")} /> */}
         </form>
       </div>
     </div>
@@ -184,5 +209,5 @@ export default connect(mapStateToProps, {
   searchGoogle,
   askCategory,
   categoryList,
-  askQuestion
+  askQuestion,
 })(Chat);
--- src/components/chat/Question.tsx
+++ src/components/chat/Question.tsx
@@ -1,67 +1,146 @@
-function Question(props:any) {
-    switch(props.question) {
-        // Password ================================================================================= 
-        case 'How can I reset my password?':
-          return (
-            <div>Answer: 
-                <ul>
-                    <li>If you know your password - 
-                        <a target="_blank" href="https://intern.fontys.nl/wwselfservice/ChangePassword.aspx">Wijzigen wachtwoord (fontys.nl)</a></li>
-                    <li>If you don't know your password <br></br>- call via WhatsApp to ServiceDesk IT or visit the student desk of your education.</li>
-                </ul>
-            </div>
-          );
-        
-        // Office 365 =================================================================================
-        case 'What is included in Office 365?':
-            return (
-                <div>Answer: <br/>The Office version we provide contains Word, Excel and Powerpoint.</div>
-            );
-        case 'Can I get Office 365?':
-            return (
-                <div>Answer: <br/>You can install in on 5 separate devices via <a target="_blank" href="https://login.microsoftonline.com/">portal.office.com</a> </div>
-            );
-        case 'Can I add an email account?':
-            return (
-                <div>Answer: <br/>You can only add email accounts which support Exchange (no pop3 or imap).</div>
-            );
-        // case 'How can I enable presentation mode (PowerPoint)?':
-        //     return (
-        //         <div>Answer: <br/>To use powerpoint in presenter mode click on ... (no answer)</div>
-        //     );
-        case 'Other Office 365 questions.':
-            return (
-                <div>Answer: <br/>Visit <a target="_blank" href="https://connect.fontys.nl/diensten/IT/Paginas/Office-365.aspx#english">Office 365 (fontys.nl)</a></div>
-            );
+import { answerRedirect } from "../../utils/ExternalRouter";
 
-        // Equipment =================================================================================
-        // case 'I have direct access problems in the start up of the laptop.':
-        //     return (
-        //         <div>Answer: <br/>No answer.</div>
-        //     );
-        case 'I have Battery, Sound or Camera issues.':
-            return (
-                <div>Answer: <br/>Please visit the IT service team on your campus.</div>
-            );
+function Question(props: any) {
+  switch (props.question) {
+    // Password =================================================================================
+    case "How can I reset my password?":
+      return (
+        <div>
+          Answer:
+          <a
+            className="google-button"
+            target="_blank"
+            href="https://google.com/search?q=Fontys+Service+Desk"
+          >
+            Search answer
+          </a>
+          <ul>
+            <li>
+              If you know your password -
+              <a
+                target="_blank"
+                href="https://intern.fontys.nl/wwselfservice/ChangePassword.aspx"
+              >
+                Wijzigen wachtwoord (fontys.nl)
+              </a>
+            </li>
+            <li>
+              If you don't know your password <br></br>- call via WhatsApp to
+              ServiceDesk IT or visit the student desk of your education.
+            </li>
+          </ul>
+        </div>
+      );
 
-        // WiFi =================================================================================    
-        case 'How to connect to eduroam (students)?':
-            return (
-                <div>Answer: <br/>See the website: <a target="_blank" href="https://connect.fontys.nl/diensten/IT/handleidingen/Paginas/verbindenmeteduroam.aspx">Handleiding Verbinden met Eduroam (fontys.nl) (Dutch)</a></div>
-            );
-        // case 'How to connect to eduroam as a visitor (guest lecturer)?':
-        //     return (
-        //         <div>Answer: <br/>No answer.</div>
-        //     );
+    // Office 365 =================================================================================
+    case "What is included in Office 365?":
+      return (
+        <div>
+          Answer:
+          <a
+            className="google-button"
+            target="_blank"
+            href="https://google.com/search?q=Fontys+Microsoft+Office"
+          >
+            Search answer
+          </a>
+          <br />
+          The Office version we provide contains Word, Excel and Powerpoint.
+        </div>
+      );
+    case "Can I get Office 365?":
+      return (
+        <div>
+          Answer: <br />
+          You can install it on 5 separate devices via{" "}
+          <a target="_blank" href="https://login.microsoftonline.com/">
+            portal.office.com
+          </a>{" "}
+        </div>
+      );
+    case "Can I add an email account?":
+      return (
+        <div>
+          Answer: <br />
+          You can only add email accounts which support Exchange (no pop3 or
+          imap).
+        </div>
+      );
+    // case 'How can I enable presentation mode (PowerPoint)?':
+    //     return (
+    //         <div>Answer: <br/>To use powerpoint in presenter mode click on ... (no answer)</div>
+    //     );
+    case "Other Office 365 questions.":
+      return (
+        <div>
+          Answer: <br />
+          Visit{" "}
+          <a
+            target="_blank"
+            href="https://connect.fontys.nl/diensten/IT/Paginas/Office-365.aspx#english"
+          >
+            Office 365 (fontys.nl)
+          </a>
+        </div>
+      );
 
-        // Audio & Video =================================================================================    
-        case 'How to use the smart board?':
-            return (
-                <div>Answer: <br/>See the website: <a target="_blank" href="https://connect.fontys.nl/diensten/IT/handleidingen/Paginas/Audio_visuele_middelen.aspx">Pagina's - Over Audio visuele middelen (fontys.nl) (Dutch)</a></div>
-            );
-        default:
-          return(null);
-      }
+    // Equipment =================================================================================
+    // case 'I have direct access problems in the start up of the laptop.':
+    //     return (
+    //         <div>Answer: <br/>No answer.</div>
+    //     );
+    case "I have Battery, Sound or Camera issues.":
+      return (
+        <div>
+          Answer:
+          <a
+            className="google-button"
+            target="_blank"
+            href="https://google.com/search?q=Fontys+Service+Desk"
+          >
+            Search answer
+          </a>
+          <br />
+          Please visit the IT service team on your campus.
+        </div>
+      );
+
+    // WiFi =================================================================================
+    case "How to connect to eduroam (students)?":
+      return (
+        <div>
+          Answer: <br />
+          See the website:{" "}
+          <a
+            target="_blank"
+            href="https://connect.fontys.nl/diensten/IT/handleidingen/Paginas/verbindenmeteduroam.aspx"
+          >
+            Handleiding Verbinden met Eduroam (fontys.nl) (Dutch)
+          </a>
+        </div>
+      );
+    // case 'How to connect to eduroam as a visitor (guest lecturer)?':
+    //     return (
+    //         <div>Answer: <br/>No answer.</div>
+    //     );
+
+    // Audio & Video =================================================================================
+    case "How to use the smart board?":
+      return (
+        <div>
+          Answer: <br />
+          See the website:{" "}
+          <a
+            target="_blank"
+            href="https://connect.fontys.nl/diensten/IT/handleidingen/Paginas/Audio_visuele_middelen.aspx"
+          >
+            Pagina's - Over Audio visuele middelen (fontys.nl) (Dutch)
+          </a>
+        </div>
+      );
+    default:
+      return null;
+  }
 }
 
 export default Question;
